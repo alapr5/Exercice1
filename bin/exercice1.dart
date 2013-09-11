e2d7764e357cee1a9c891eae20b496c4d36d5c7d
Dzenan Ridjanovic <dzenanr@gmail.com>
2013-09-11 14:56:09 -0400

    format code; add doc

--- bin/exercice1.dart
+++ bin/exercice1.dart
@@ -1,25 +1,21 @@
-import 'dart:math';
-
 void main() {
-
-  var i=0; //number
-  var a=0;
+  var i = 0;
+  var a = 0;
   print("Write a program that prints the numbers from 1 to 100. But for multiples of three print “Fizz” instead of the number and for the multiples of five print “Buzz”. For numbers which are multiples of both three and five print “FizzBuzz”.");
-  for (int i=1;i<=100;i++){
-    if (i%15 == 0){
+  for (int i = 1; i <= 100; i++) {
+    if (i % 15 == 0) {
       print("FizzBuzz");
-    }else{
-       if (i%3 == 0){
+    } else {
+       if (i % 3 == 0){
          print("Fizz");
-       }else if (i%5 == 0){
+       } else if (i % 5 == 0){
          print("Buzz");
-       }else {
+       } else {
          print("$i");
        }
     }
   }
 }
-
 
 // Modulo : http://api.dartlang.org/docs/releases/latest/dart_core/num.html
 // exemple de if : https://www.dartlang.org/docs/dart-up-and-running/contents/ch02.html#if-else
